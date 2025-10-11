bfe3df736017c0e0735af53650c62c83c39073b7
Thomas Shrader <64pigpig2014@gmail.com>
2025-10-11 11:36:34 -0400

    removed "Green flag!' alert on green flag

--- src/objects.js
+++ src/objects.js
@@ -10988,8 +10988,6 @@ StageMorph.prototype.fireUserEditEvent = function (
 };
 
 StageMorph.prototype.fireGreenFlagEvent = function () {
-    alert("Green flag!");
-
     var procs = [],
         ide = this.parentThatIsA(IDE_Morph);
 
